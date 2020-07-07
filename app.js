b9ce2df57e63024451574fb78b182bba5b964834
EdgarPDX <66703437+EdgarPDX@users.noreply.github.com>
2020-07-07 13:58:49 -0700

    added DOMS

---
+++ app.js
@@ -0,0 +1,17 @@
+// import functions 
+
+//DOM elements
+const input = document.getElementById('user-input');
+const button = document.getElementById('button');
+const attemptsLeft = document.getElementById('attempts-left');
+const hotOrCold = document.getElementById('hot-cold');
+const result = document.getElementById('win-loss');
+
+// initialize state
+
+
+// set event listeners to update state and DOM
+
+
+
+})
